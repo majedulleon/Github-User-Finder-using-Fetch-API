a715f878e0cb96cdae829acad61db760f344fa06
Majedul Islam Leon <majedulleon@gmail.com>
2022-10-11 19:55:08 +0600

    Finished at Last

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
 let searchBtn = document.querySelector('#searchBtn');
 let searchUser = document.querySelector('#searchUser');
+let ui = new UI();
 
 searchBtn.addEventListener('click', (e) => {
-    let userText = searchUser.Value;
+    let userText = searchUser.value;
     if (userText != '') {
         //Fetch API Use here
         fetch(`https://api.github.com/users/${userText}`)
             .then(result => result.json())
             .then(data => {
-                if (data.messege == 'Not Found') {
+                if (data.message == 'Not Found') {
                     //Show Alert
+                    ui.showAlert("User Not Available", "alert alert-danger");
                 }
                 else {
                     //Show User Info
+                    ui.showProfile(data);
                 }
             })
     }
     else {
         //Clear Profile
+        ui.clearProfile();
     }
 });
